abca186b4506d12e2546329f89082701c7419b53
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2022-07-11 20:18:07 -0400

    Major W's

--- pages/buble.vue
+++ pages/buble.vue
@@ -3,12 +3,46 @@ import { useArrStore } from "~/store";
 const arrStore = useArrStore();
 
 onMounted(() => {
-    arrStore.randomiseArr();
+    arrStore.genNewArr();
+    // arrStore.$onAction(
+    // ({ name, args, store, after, onError }) => {
+    //     console.log(name.toString() === "randomiseArr");
+    // })
+
 })
 
-const doBubleSort = () => {
-    if (arrStore.sortable) arrStore.bubleSort(arrStore.array);
+// const timeout = (ms: number) => {
+//     return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// const bubleSort = (arr: Array<number>) => {
+//     arrStore.sortable = false;
+//     for (let i = 0; i < arrStore.arrLen; i++) {
+//         for (let j = 0; j < arrStore.arrLen - i - 1; j++) {
+//             console.log("happenin");
+//             if (arr[j] > arr[j + 1]) {
+//                 arrStore.animationsIndx.swap_1 = j;
+//                 arrStore.animationsIndx.swap_2 = j + 1;
+//                 let temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//                 timeout(arrStore.sortSpeed);
+//                 arrStore.animationsIndx.swap_1 = null;
+//                 arrStore.animationsIndx.swap_2 = null;
+//             }
+//         }
+//         arrStore.animationsIndx.done.push(arrStore.arrLen - 1 - i);
+//     }
+//     arrStore.sortable = true;
+// }
+
+
+const genNewArrAndStopSort = () => {
+    // arrStore.stopSort = true;
+    // arrStore.sortable = true;
+    arrStore.randomiseArr();
 }
+
 </script>
 
 <template>
@@ -16,7 +50,7 @@ const doBubleSort = () => {
         <h1>{{ arrStore.array }}</h1>
         <ArrayTable></ArrayTable>
         <div class="flex gap-10 mt-10">
-            <button class="btn btn-outline" @click="doBubleSort">Sort</button>
+            <button class="btn btn-outline" @click="arrStore.bubleSort()">Sort</button>
             <button class="btn btn-outline" @click="arrStore.randomiseArr()">Randomize</button>
         </div>
     </div>
--- store.ts
+++ store.ts
@@ -14,14 +14,16 @@ interface State {
     done: number[];
   };
   sortable: boolean;
+  stopSort: boolean;
 }
 
 interface Getters {}
 
 interface Actions {
   randomiseArr(): void;
+  genNewArr(): void;
   timeout(ms: number): void;
-  bubleSort(arr: Array<number>): void;
+  bubleSort(): void;
 }
 
 export const useArrStore = defineStore<"array-store", State, {}, Actions>(
@@ -33,17 +35,21 @@ export const useArrStore = defineStore<"array-store", State, {}, Actions>(
         arrLen: 80,
         min: 10,
         max: 180,
-        sortSpeed: 10, // in ms
+        sortSpeed: 5, // in ms
         animationsIndx: {
           swap_1: null,
           swap_2: null,
           done: [],
         },
         sortable: true,
+        stopSort: false,
       };
     },
     actions: {
-      randomiseArr() {
+      timeout(ms: number) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+      },
+      genNewArr() {
         this.array = [];
         this.animationsIndx = {
           swap_1: null,
@@ -56,28 +62,47 @@ export const useArrStore = defineStore<"array-store", State, {}, Actions>(
           );
         }
       },
-      timeout(ms: number) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-      },
-      async bubleSort(arr: Array<number>) {
-        this.sortable = false;
-        for (let i = 0; i < this.arrLen; i++) {
-          for (let j = 0; j < this.arrLen - i - 1; j++) {
-            console.log("happenin");
-            if (arr[j] > arr[j + 1]) {
-              this.animationsIndx.swap_1 = j;
-              this.animationsIndx.swap_2 = j + 1;
-              let temp = arr[j];
-              arr[j] = arr[j + 1];
-              arr[j + 1] = temp;
-              await this.timeout(this.sortSpeed);
-              this.animationsIndx.swap_1 = null;
-              this.animationsIndx.swap_2 = null;
-            }
-          }
-          this.animationsIndx.done.push(this.arrLen - 1 - i);
-        }
+      randomiseArr() {
+        this.stopSort = true;
         this.sortable = true;
+        this.array = [];
+        this.animationsIndx = {
+          swap_1: null,
+          swap_2: null,
+          done: [],
+        };
+        for (let i = 0; i < this.arrLen; i++) {
+          this.array.push(
+            Math.floor(Math.random() * (this.max - this.min + 1) + this.min)
+          );
+        }
+      },
+      async bubleSort() {
+        if (this.sortable) {
+          this.stopSort = false;
+          console.log(this.stopSort);
+          this.sortable = false;
+          for (let i = 0; i < this.arrLen; i++) {
+            for (let j = 0; j < this.arrLen - i - 1; j++) {
+              if (this.array[j] > this.array[j + 1]) {
+                if (this.stopSort === true) {
+                  console.log(this.stopSort);
+                  return;
+                }
+                this.animationsIndx.swap_1 = j;
+                this.animationsIndx.swap_2 = j + 1;
+                let temp = this.array[j];
+                this.array[j] = this.array[j + 1];
+                this.array[j + 1] = temp;
+                await this.timeout(this.sortSpeed);
+                this.animationsIndx.swap_1 = null;
+                this.animationsIndx.swap_2 = null;
+              }
+            }
+            this.animationsIndx.done.push(this.arrLen - 1 - i);
+          }
+          this.sortable = true;
+        }
       },
     },
   }
