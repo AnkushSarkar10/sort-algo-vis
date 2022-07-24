89f2883852202b35f287414873653a57d3c3a249
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2022-07-24 03:57:57 -0400

    switched anchor tags for NuxtLinks

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,26 +1,23 @@
 <script setup>
 const navItemsClass = "hover:btn-primary mx-2";
 const activeClass = "btn bg-base-200 text-primary font-semibold mt-1";
-// const activeClass = "tab tab-lg bg-base-300 tab-active text-primary font-semibold";
+
 const route = useRoute();
-// switch (route.name) {
-//   case "buble"
-// }
 
 </script>
 
 <template>
     <div class="navbar bg-base-100">
         <div class="flex-1">
-            <a class="ml-4 px-4 py-2 text-primary text-xl hover:btn-primary rounded-lg" href="/">Home</a>
+            <NuxtLink class="ml-4 px-4 py-2 text-primary text-xl hover:btn-primary rounded-lg" href="/">Home</NuxtLink>
         </div>
         <div class="flex-none">
             <ul class="menu menu-horizontal p-0 tabs">
-                <li><a :class="[route.name == 'buble' ? activeClass : '' ,navItemsClass]" href="/buble">Buble Sort</a></li>
-                <li><a :class="[route.name == 'merge'  ? activeClass : '' ,navItemsClass]" href="/merge">Merge Sort</a></li>
-                <li><a :class="[route.name == 'quick'  ? activeClass : '' ,navItemsClass]" href="/quick">Quick Sort</a></li>
-                <li><a :class="[route.name == 'insertion'  ? activeClass : '' ,navItemsClass]" href="/insertion">Insertion Sort</a></li>
-                <li><a :class="[route.name == 'selection'  ? activeClass : '' ,navItemsClass]" href="/selection">Selection Sort</a></li>
+                <li><NuxtLink a :class="[route.name == 'buble' ? activeClass : '' ,navItemsClass]" href="/buble">Buble Sort</NuxtLink></li>
+                <li><NuxtLink :class="[route.name == 'merge'  ? activeClass : '' ,navItemsClass]" href="/merge">Merge Sort</NuxtLink></li>
+                <li><NuxtLink :class="[route.name == 'quick'  ? activeClass : '' ,navItemsClass]" href="/quick">Quick Sort</NuxtLink></li>
+                <li><NuxtLink :class="[route.name == 'insertion'  ? activeClass : '' ,navItemsClass]" href="/insertion">Insertion Sort</NuxtLink></li>
+                <li><NuxtLink :class="[route.name == 'selection'  ? activeClass : '' ,navItemsClass]" href="/selection">Selection Sort</NuxtLink></li>
             </ul>
         </div>
     </div>
